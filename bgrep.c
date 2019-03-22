009ec40cd5ac16e37c38fbdebcf05b773abb6c72
xerub <xerub@aurora>
2019-03-22 01:58:56 +0000

    flippin teh burgers

--- bgrep.c
+++ bgrep.c
@@ -17,6 +17,7 @@
  */
 
 
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,6 +25,16 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
+#ifdef ZE_BIG_ENDIAN
+#define SWAP64(x) __builtin_bswap64(x)
+#define SWAP32(x) __builtin_bswap32(x)
+#define SWAP16(x) __builtin_bswap16(x)
+#else
+#define SWAP64(x) x
+#define SWAP32(x) x
+#define SWAP16(x) x
+#endif
 
 #define UCHAR_MAX 255
 
@@ -83,19 +94,28 @@ boyermoore_horspool_memmem(const unsigned char* haystack, size_t hlen,
 }
 
 static size_t
-str2hex(size_t buflen, unsigned char *buf, unsigned char *mask, const char *str)
+str2hex(size_t buflen, unsigned char *buf, unsigned char *mask, const char *str, int width)
 {
     unsigned char *ptr = buf;
-    int seq = -1;
-    int m = 0;
-    while (buflen > 0) {
+    unsigned long long acc = 0;
+    unsigned long long m = 0;
+    int seq = 0;
+    switch (width) {
+        case 8:
+        case 4:
+        case 2:
+            break;
+        default:
+            width = 1;
+    }
+    while (buflen >= width) {
         int nibble = *str++;
         if (nibble >= '0' && nibble <= '9') {
             nibble -= '0';
             m |= 0xF;
         } else if (nibble == '.') {
             nibble = 0;
-        } else if (nibble == ' ' && seq < 0) {
+        } else if (nibble == ' ' && seq == 0) {
             continue;
         } else {
             nibble |= 0x20;
@@ -105,16 +125,35 @@ str2hex(size_t buflen, unsigned char *buf, unsigned char *mask, const char *str)
             nibble -= 'a' - 10;
             m |= 0xF;
         }
-        if (seq >= 0) {
-            *buf++ = (seq << 4) | nibble;
-            *mask++ = m;
-            buflen--;
+        acc = (acc << 4) | nibble;
+        if (seq == width * 2 - 1) {
+            switch (width) {
+                case 8:
+                    *(unsigned long long *)buf = SWAP64(acc);
+                    *(unsigned long long *)mask = SWAP64(m);
+                    break;
+                case 4:
+                    *(unsigned int *)buf = SWAP32(acc);
+                    *(unsigned int *)mask = SWAP32(m);
+                    break;
+                case 2:
+                    *(unsigned short *)buf = SWAP16(acc);
+                    *(unsigned short *)mask = SWAP16(m);
+                    break;
+                case 1:
+                    *(unsigned char *)buf = acc;
+                    *(unsigned char *)mask = m;
+                    break;
+            }
+            buf += width;
+            mask += width;
+            buflen -= width;
             seq = -1;
+            acc = 0;
             m = 0;
-        } else {
-            seq = nibble;
-            m <<= 4;
         }
+        m <<= 4;
+        seq++;
     }
     return buf - ptr;
 }
@@ -143,8 +182,8 @@ find_sequence(const unsigned char *mask, size_t n, size_t *len)
     return best_pos - best_len;
 }
 
-unsigned char *
-process_pattern(const char *str, size_t *len, unsigned char **out_mask, size_t *seq_pos, size_t *seq_len)
+static unsigned char *
+process_pattern(const char *str, int width, size_t *len, unsigned char **out_mask, size_t *seq_pos, size_t *seq_len)
 {
     size_t n = strlen(str) / 2;
     unsigned char *pattern, *mask;
@@ -162,7 +201,7 @@ process_pattern(const char *str, size_t *len, unsigned char **out_mask, size_t *
         return NULL;
     }
 
-    n = str2hex(n, pattern, mask, str);
+    n = str2hex(n, pattern, mask, str, width);
     if (!n) {
         free(mask);
         free(pattern);
@@ -217,7 +256,7 @@ find_string(const unsigned char* haystack, size_t hlen,
 }
 
 static int
-bgrep(const char *filename, const unsigned char *pattern, size_t len, const unsigned char *mask, ssize_t seq_pos, size_t seq_len, int *any, int fn)
+bgrep(const char *filename, const unsigned char *pattern, size_t len, const unsigned char *mask, ssize_t seq_pos, size_t seq_len, long *any, long max, int show_fnames)
 {
     int rv;
     int fd;
@@ -245,53 +284,112 @@ bgrep(const char *filename, const unsigned char *pattern, size_t len, const unsi
     }
 
     ptr = p - 1;
-    while (1) {
+    while (max < 0 || *any < max) {
         size_t left = st.st_size - (++ptr - p);
         ptr = find_string(ptr, left, pattern, len, mask, seq_pos, seq_len);
         if (!ptr) {
             break;
         }
-        if (fn) {
+        if (show_fnames) {
             printf("%s: ", filename);
         }
         printf("0x%zx\n", ptr - p);
-        *any = 1;
+        (*any)++;
     }
 
     munmap((void *)p, st.st_size);
     return 0;
 }
 
+static const char *
+parse_longarg(int *argc, char ***argv, const char *q, long *pval)
+{
+    long val;
+    char *bp;
+    if (!*q) {
+        if (*argc < 2) {
+            return NULL;
+        }
+        (*argc)--;
+        (*argv)++;
+        q = **argv;
+    }
+    errno = 0;
+    val = strtol(q, &bp, 10);
+    if (errno || bp == q || *bp) {
+        return NULL;
+    }
+    *pval = val;
+    return bp - 1;
+}
+
 int
 main_bgrep(int argc, char **argv)
 {
     int rv;
-    int fn;
+    int show_fnames;
     const char *str;
+    const char *myself = argv[0];
     size_t len, seq_len, seq_pos;
     unsigned char *pattern, *mask;
-    int any = 0;
+    long max_match_count = -1;
+    long any = 0;
+    int width = 1;
 
-    if (argc < 3) {
-        fprintf(stderr, "usage: %s PATTERN FILE...\n", argv[0]);
+    while (--argc > 0) {
+        const char *p = *++argv;
+        if (!strcmp(p, "-16")) {
+            width = 2;
+            continue;
+        }
+        if (!strcmp(p, "-32")) {
+            width = 4;
+            continue;
+        }
+        if (p[0] == '-') {
+            const char *q;
+            for (q = ++p; *q; q++) {
+                switch (*q) {
+                    case 'm':
+                        q = parse_longarg(&argc, &argv, q + 1, &max_match_count);
+                        if (!q) {
+                            goto usage;
+                        }
+                        continue;
+                }
+                break;
+            }
+            if (*q || q == p) {
+                goto usage;
+            }
+            continue;
+        }
+        break;
+    }
+
+    if (argc < 2) usage: {
+        fprintf(stderr, "usage: %s [OPTIONS] PATTERN FILE...\n"
+                        "    -16 treat input as sequence of 16bit words (swapped)\n"
+                        "    -32 treat input as sequence of 32bit words (swapped)\n"
+                        "    -m  max # of matches\n", myself);
         fprintf(stderr, "notes:\n"
                 "    PATTERN consists of a series of hexadecimal bytes: ab cd\n"
                 "    Spaces between bytes are ignored.  Hexdigit wildcard is .\n");
         return 2;
     }
 
-    str = argv[1];
-    pattern = process_pattern(str, &len, &mask, &seq_pos, &seq_len);
+    str = argv[0];
+    pattern = process_pattern(str, width, &len, &mask, &seq_pos, &seq_len);
     if (!pattern) {
         fprintf(stderr, "error processing pattern\n");
         return -1;
     }
 
     rv = 0;
-    fn = argc > 3;
-    while (argc-- > 2) {
-        int rc = bgrep((argv++)[2], pattern, len, mask, seq_pos, seq_len, &any, fn);
-        if (rc < 0) {
+    show_fnames = argc > 2;
+    while (argc-- > 1) {
+        int rc = bgrep((argv++)[1], pattern, len, mask, seq_pos, seq_len, &any, max_match_count, show_fnames);
+        if (rc) {
             rv = rc;
         }
     }
